6d22d9ed55e285ce41ae87608c0e6992278580e7
KowshickChowdhury <kowshick1170@gmail.com>
2025-01-13 17:22:14 +0600

    Merge branch 'anamul' of https://github.com/backbencherstudio/Travel-agency-client into kowshick

---
+++ src/Pages/Cruises/AllCruises.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useRef, useState } from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+import { CiSearch } from 'react-icons/ci';
+import { FaRegCalendarAlt, FaStar } from 'react-icons/fa';
+import TourCard from './tourCard';
+import package1 from "../../assets/img/travel-packages/package-1.png";
+import package2 from "../../assets/img/travel-packages/package-2.png";
+import package3 from "../../assets/img/travel-packages/package-3.png";
+import ClientPackageApis from '../../Apis/clientApi/ClientPackageApis';
+
+const AllCruises = () => {
+  return (
+    <div>AllCruises</div>
+  )
+}
+
+export default AllCruises
