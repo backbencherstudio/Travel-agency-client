f37b69d9e56bc43b3c6c5d01daf9ff2d628bdca8
KowshickChowdhury <kowshick1170@gmail.com>
2025-06-07 17:04:45 +0600

    updated image modal

--- src/Components/DetailsComponent/ImageModal.jsx
+++ src/Components/DetailsComponent/ImageModal.jsx
@@ -2,18 +2,18 @@ import React from 'react'
 
 const ImageModal = ({ showImageModal, setShowImageModal, images, modalImageIndex, setModalImageIndex, activeTab, setActiveTab, details }) => {
     return (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-[#232a2f]">
-            <div className="relative w-full h-full max-w-7xl flex flex-col justify-between items-center">
-                <div className='flex flex-row justify-between items-center py-4 w-full'>
-                    <div className='flex flex-row justify-between items-center'>
-                        <p>Huacachha Oats & Most reviewed company in Peru!</p>
-                        <button className='bg-white text-black px-4 py-2 rounded-md'>Check Availability</button>
-                    </div>
-                    {/* Close Button */}
-                    <button onClick={() => setShowImageModal(false)} className=" text-gray-400 text-2xl">
-                        <span className='flex items-center px-2 bg-gray-100 rounded-full'>&times;</span>
-                    </button>
+        <div className="fixed inset-0 z-50  flex flex-col justify-between items-center bg-[#232a2f]">
+            <div className='flex flex-row justify-center items-center py-5 w-full relative border-b border-[#A5A5AB]'>
+                <div className='flex flex-row justify-between items-center gap-8'>
+                    <p className='font-medium text-white'>Huacachha Oats & Most reviewed company in Peru!</p>
+                    <button className='bg-[#EB5B2A] text-sm font-semibold text-white px-4 py-2 rounded-full'>Check Availability</button>
                 </div>
+                {/* Close Button */}
+                <button onClick={() => setShowImageModal(false)} className=" text-gray-400 text-2xl absolute right-0">
+                    <span className='flex items-center px-2 bg-gray-100 rounded-full'>&times;</span>
+                </button>
+            </div>
+            <div className="relative w-full h-full max-w-7xl py-8">
                 {/* Tabs */}
                 <div className="flex justify-center mb-4">
                     <button
