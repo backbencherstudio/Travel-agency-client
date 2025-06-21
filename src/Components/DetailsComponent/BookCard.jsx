c12e4fb4f83a7e009a149409483dca870b0aaec8
Miraz Hossain <105309402+Miraz661@users.noreply.github.com>
2025-06-21 17:15:56 +0600

    Tour details section completed.

--- src/Components/DetailsComponent/BookCard.jsx
+++ src/Components/DetailsComponent/BookCard.jsx
@@ -18,6 +18,8 @@ const BookCard = ({ details, renderStars }) => {
   const startDatePickerRef = useRef(null)
   const endDatePickerRef = useRef(null)
   const navigate = useNavigate()
+  const [freeCancellation, setFreeCancellation] = useState(false)
+  const [payLater,setPayLater] = useState(false)
 
   // Access user from AuthContext
   const { user } = useContext(AuthContext)
@@ -57,6 +59,14 @@ const BookCard = ({ details, renderStars }) => {
     }
   }
 
+  const handleFreeChancellation = () => {
+    setFreeCancellation(prev => !prev)
+  }
+
+  const handlePayLater=()=>{
+    setPayLater(prev=> !prev)
+  }
+
   const handleBookNow = async () => {
     if (!user) {
       toast.error('You need to log in to proceed with booking.')
@@ -87,7 +97,7 @@ const BookCard = ({ details, renderStars }) => {
     try {
       setLoading(true)
       document.body.style.overflow = 'hidden'
-      
+
       setTimeout(async () => {
         try {
           const response = await createCheckout(bookingData)
@@ -134,14 +144,29 @@ const BookCard = ({ details, renderStars }) => {
           <Loading />
         </div>
       )}
-      <div className='flex flex-col gap-4 max-w-full'>
-        <h1 className='text-[40px] font-bold border-b border-b-[#e5e6e6] pb-[15px]'>
+      <div className='flex flex-col gap-4 max-w-full shadow-[0px_5px_30px_0px_#00000014] p-6 rounded-xl'>
+        <h1 className='text-[40px] font-bold border-b border-b-[#A6AAAC33] pb-[15px]'>
           ${details?.price}
           <span className='text-lg font-normal'>/per person</span>
         </h1>
+        <div className='text-[#000] text-xl font-semibold'>Select Date and Travelers</div>
         <div>
           {/* Start Date Picker */}
-          <div className='flex border items-center justify-between p-2 rounded-md border-[#e5e6e6] shadow-sm'>
+          <div className='flex border items-center gap-4 p-4 rounded-md border-[#e5e6e6] shadow-sm'>
+
+            <div onClick={() => startDatePickerRef.current.setOpen(true)}
+              className='text-2xl cursor-pointer ml-2 w-fit'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="25" height="24" viewBox="0 0 25 24" fill="none">
+                <path d="M3.25 7.5C3.25 5.29086 5.04086 3.5 7.25 3.5H17.25C19.4591 3.5 21.25 5.29086 21.25 7.5V18C21.25 20.2091 19.4591 22 17.25 22H7.25C5.04086 22 3.25 20.2091 3.25 18V7.5Z" stroke="#0F1416" stroke-width="1.5" />
+                <path d="M3.25 9H21.25" stroke="#0F1416" stroke-width="1.5" stroke-linecap="round" />
+                <path d="M8.25 2L8.25 5" stroke="#0F1416" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M16.25 2V5" stroke="#0F1416" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <circle cx="12.25" cy="15" r="1" fill="#0F1416" />
+                <circle cx="16.25" cy="15" r="1" fill="#0F1416" />
+                <circle cx="8.25" cy="15" r="1" fill="#0F1416" />
+              </svg>
+            </div>
+
             <DatePicker
               selected={startDate}
               onChange={handleStartDateChange}
@@ -153,15 +178,19 @@ const BookCard = ({ details, renderStars }) => {
               className='outline-none w-full placeholder:text-[#b6b9bb] placeholder:text-base placeholder:font-normal'
               ref={startDatePickerRef}
             />
-            <img
-              src={calender}
-              onClick={() => startDatePickerRef.current.setOpen(true)}
-              className='text-2xl cursor-pointer ml-2'
-            />
           </div>
 
           {/* End Date Picker */}
-          <div className='flex border mt-4 items-center justify-between p-2 rounded-md border-[#e5e6e6] shadow-sm'>
+          <div className='flex border mt-4 items-center gap-4 p-4 rounded-md border-[#e5e6e6] shadow-sm'>
+
+            <div onClick={() => endDatePickerRef.current.setOpen(true)}
+              className='text-2xl cursor-pointer ml-2 w-fit'>
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="22" viewBox="0 0 20 22" fill="none">
+                <path d="M4.82757 14.4816C3.4128 15.324 -0.296635 17.0441 1.96266 19.1966C3.06631 20.248 4.29549 21 5.84087 21H14.6591C16.2045 21 17.4337 20.248 18.5373 19.1966C20.7966 17.0441 17.0872 15.324 15.6724 14.4816C12.3548 12.5061 8.14519 12.5061 4.82757 14.4816Z" stroke="#1D1F2C" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                <path d="M14.75 5.5C14.75 7.98528 12.7353 10 10.25 10C7.76472 10 5.75 7.98528 5.75 5.5C5.75 3.01472 7.76472 1 10.25 1C12.7353 1 14.75 3.01472 14.75 5.5Z" stroke="#1D1F2C" stroke-width="1.5" />
+              </svg>
+            </div>
+
             <DatePicker
               selected={endDate}
               onChange={handleEndDateChange}
@@ -171,19 +200,14 @@ const BookCard = ({ details, renderStars }) => {
               minDate={startDate}
               maxDate={calculateMaxEndDate(startDate)}
               placeholderText='End Date'
-              className='outline-none w-full placeholder:text-[#b6b9bb] placeholder:text-base placeholder:font-normal'
+              className='outline-none text-base w-full placeholder:text-[#b6b9bb] placeholder:text-base placeholder:font-normal'
               ref={endDatePickerRef}
               disabled={!startDate}
-            />
-            <img
-              src={calender}
-              onClick={() => endDatePickerRef.current.setOpen(true)}
-              className='text-2xl cursor-pointer ml-2'
             />
           </div>
 
           {/* Extra Services */}
-          <div className='flex flex-col gap-4 mt-5'>
+          {/* <div className='flex flex-col gap-4 mt-5'>
             <h4 className='text-xl font-bold text-[#0F1416]'>Extra Service</h4>
             {[
               ...new Map(
@@ -203,15 +227,59 @@ const BookCard = ({ details, renderStars }) => {
                 </p>
               </div>
             ))}
-          </div>
+          </div> */}
         </div>
 
         <button
           onClick={handleBookNow}
           className='flex gap-2 items-center justify-center p-3 bg-[#EB5B2A] rounded-full text-white text-base font-medium w-full mt-2'
         >
-          Book Now
+          Check Availability
         </button>
+        <div className='flex flex-col gap-4 text-[#49556D] bg-[#FDEFEAB2] p-4 rounded-xl'>
+          <div className='flex gap-[10px]'>
+            <div className='relative w-[24px] h-[24px] text-white flex gap-[10px]'>
+              <input type="checkbox" checked={freeCancellation} onClick={handleFreeChancellation} class="w-6 h-6 border-2 border-gray-400 rounded-full appearance-none checked:bg-[#14AE5C] checked:border-[#14AE5C] focus:outline-none" />
+              <div className='absolute top-0 flex items-center justify-center' onClick={handleFreeChancellation}>
+                <svg
+                  className="w-full h-full"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M20.285 6.709l-11.02 11.02-5.657-5.657 1.414-1.415 4.243 4.243L18.87 5.294l1.415 1.415z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <p className='text-[#49556D] text-sm '><span className='text-[#49556D] underline cursor-pointer text-nowrap font-bold text-sm leading-5'>Free Cancellation</span> up to 24 hours before the experience starts (local time)</p>
+            </div>
+          </div>
+          <div className='flex gap-[10px]'>
+            <div className='relative w-[24px] h-[24px] text-white flex gap-[10px]'>
+              <input type="checkbox" checked={payLater} onClick={handlePayLater} class="w-6 h-6 border-2 border-gray-400 rounded-full appearance-none checked:bg-[#14AE5C] checked:border-[#14AE5C] focus:outline-none" />
+              <div className='absolute top-0 flex items-center justify-center' onClick={handlePayLater}>
+                <svg
+                  className="w-full h-full"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M20.285 6.709l-11.02 11.02-5.657-5.657 1.414-1.415 4.243 4.243L18.87 5.294l1.415 1.415z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <div>
+              <span className='underline cursor-pointer text-[#49556D] text-nowrap text-sm font-bold'>Book Now and Pay Leter</span>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
--- src/Pages/Tours/TourDetails/TourDetails.jsx
+++ src/Pages/Tours/TourDetails/TourDetails.jsx
@@ -17,48 +17,48 @@ import Loading from "../../../Shared/Loading";
 import { Helmet } from "react-helmet-async";
 
 const TourDetails = () => {
-    const links = [{name: "Home", path: "/"}, {name: "Tours", path: "/tours"}, {name: "Tour Details", path: ""},]
-    const [loading, setLoading] = useState(true);
-    const [tour, setTour] = useState(null);
-    const { id } = useParams();
-    
-    useEffect(() => {
-      tourDetails();
-    }, [])
+  const links = [{ name: "Home", path: "/" }, { name: "Tours", path: "/tours" }, { name: "Tour Details", path: "" },]
+  const [loading, setLoading] = useState(true);
+  const [tour, setTour] = useState(null);
+  const { id } = useParams();
 
-    const tourDetails = async () => {
-      const res = await ClientPackageApis.getOne(id);
-      if (res?.success) {
-        setTour(res?.data)
-        setLoading(false);
-      }
-      // console.log('res', res)
+  useEffect(() => {
+    tourDetails();
+  }, [])
+
+  const tourDetails = async () => {
+    const res = await ClientPackageApis.getOne(id);
+    if (res?.success) {
+      setTour(res?.data)
+      setLoading(false);
     }
-        
-    // const tour =  {
-    //     id: 1,
-    //     title: "Sacred Temples of Bali",
-    //     description: "Explore our diverse range of guided tours. each designed to immerse you in captivating destinations and unforgettable experiences.",
-    //     overview: "Bali is a paradise that promises breathtaking scenery, rich cultural heritage, and unforgettable experiences. From serene beaches and ancient temples to majestic waterfalls hidden in lush landscapes, Bali has something for every traveler. This guide takes you through the must-see spots in Bali for an adventure you'll treasure forever.",
-    //     rating: 4.8,
-    //     days: 7,
-    //     location: "Beijing, China",
-    //     price: 2999,
-    //     locationImage: image1,
-    //     include: ["Hotel + All inclusive", "Breakfast, Lunch & Dinner", "Hotel Accommodation", "How to use premade UI kits", "Transfer Between Destinations"],
-    //     exclude: ["Sight-seen", "City Tour", "Custom Duty"],
-    //     tourPlan: [
-    //         {"locationImage": image1, "day": 1, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
-    //         {"locationImage": image2, "day": 2, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
-    //         {"locationImage": image3, "day": 3, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
-    //         {"locationImage": image4, "day": 4, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}
-    //     ],
-    // }
+    // console.log('res', res)
+  }
 
-    // console.log('id', id)
-    
+  // const tour =  {
+  //     id: 1,
+  //     title: "Sacred Temples of Bali",
+  //     description: "Explore our diverse range of guided tours. each designed to immerse you in captivating destinations and unforgettable experiences.",
+  //     overview: "Bali is a paradise that promises breathtaking scenery, rich cultural heritage, and unforgettable experiences. From serene beaches and ancient temples to majestic waterfalls hidden in lush landscapes, Bali has something for every traveler. This guide takes you through the must-see spots in Bali for an adventure you'll treasure forever.",
+  //     rating: 4.8,
+  //     days: 7,
+  //     location: "Beijing, China",
+  //     price: 2999,
+  //     locationImage: image1,
+  //     include: ["Hotel + All inclusive", "Breakfast, Lunch & Dinner", "Hotel Accommodation", "How to use premade UI kits", "Transfer Between Destinations"],
+  //     exclude: ["Sight-seen", "City Tour", "Custom Duty"],
+  //     tourPlan: [
+  //         {"locationImage": image1, "day": 1, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
+  //         {"locationImage": image2, "day": 2, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
+  //         {"locationImage": image3, "day": 3, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}, 
+  //         {"locationImage": image4, "day": 4, "tourDescription": "Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."}
+  //     ],
+  // }
+
+  // console.log('id', id)
+
   return (
-    <div>
+    <div className="pt-[54px] pb-[80px] px-[192px]">
       <Helmet>
         <title>Around 360 - Tour Details</title>
       </Helmet>
@@ -66,11 +66,31 @@ const TourDetails = () => {
         <Loading />
       ) : (
         <div>
-          <div className="bg-[#F0F4F9]" >
-              <HeroSection bgImg={bgImg} pageName="Our Tour" links={links} description={tour?.description} />
+          <div className="bg-[#fff] flex flex-col gap-5" >
+            {/* <HeroSection bgImg={bgImg} pageName="Our Tour" links={links} description={tour?.description} /> */}
+            <div className="max-w-[1216px] mx-auto w-full">
+              <div className="flex justify-between w-full">
+                <div className="flex items-center gap-2 text-sm">
+                  <span>Home</span> <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.942841 0.345964C0.673302 0.561595 0.629601 0.954903 0.845232 1.22444L3.86622 5.00067L0.845232 8.77691C0.629601 9.04645 0.673301 9.43975 0.94284 9.65538C1.21238 9.87102 1.60569 9.82731 1.82132 9.55778L5.15465 5.39111C5.33726 5.16285 5.33726 4.8385 5.15465 4.61024L1.82132 0.443573C1.60569 0.174034 1.21238 0.130333 0.942841 0.345964Z" fill="#0F1416" />
+                  </svg> <span>Tour</span> <svg xmlns="http://www.w3.org/2000/svg" width="6" height="10" viewBox="0 0 6 10" fill="none">
+                    <path fill-rule="evenodd" clip-rule="evenodd" d="M0.942841 0.345964C0.673302 0.561595 0.629601 0.954903 0.845232 1.22444L3.86622 5.00067L0.845232 8.77691C0.629601 9.04645 0.673301 9.43975 0.94284 9.65538C1.21238 9.87102 1.60569 9.82731 1.82132 9.55778L5.15465 5.39111C5.33726 5.16285 5.33726 4.8385 5.15465 4.61024L1.82132 0.443573C1.60569 0.174034 1.21238 0.130333 0.942841 0.345964Z" fill="#0F1416" />
+                  </svg> <span>Tour Details</span>
+                </div>
+                <div className="flex gap-1 p-2 text-[16px] items-center bg-[#F6F8FA] w-fit rounded cursor-pointer">
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" fill="none">
+                      <path d="M9.44699 13.9264C12.2357 13.741 14.457 11.4882 14.6398 8.65993C14.6755 8.10647 14.6755 7.5333 14.6398 6.97983C14.457 4.1516 12.2357 1.89877 9.44699 1.7134C8.4956 1.65016 7.5023 1.65029 6.55285 1.7134C3.76418 1.89877 1.54286 4.1516 1.36008 6.97983C1.32431 7.5333 1.32431 8.10647 1.36008 8.65993C1.42665 9.69001 1.88221 10.6438 2.41852 11.4491C2.72992 12.0129 2.52441 12.7166 2.20006 13.3312C1.96619 13.7744 1.84926 13.996 1.94315 14.1561C2.03704 14.3162 2.24676 14.3213 2.6662 14.3315C3.4957 14.3517 4.05504 14.1165 4.49904 13.7891C4.75085 13.6034 4.87676 13.5106 4.96354 13.4999C5.05032 13.4892 5.2211 13.5596 5.56259 13.7002C5.86952 13.8266 6.2259 13.9046 6.55285 13.9264C7.5023 13.9895 8.4956 13.9896 9.44699 13.9264Z" stroke="#EB5B2A" stroke-width="1.5" stroke-linejoin="round" />
+                      <path d="M7.99693 8H8.00291M10.6606 8H10.6666M5.33325 8H5.33923" stroke="#EB5B2A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                  </div>
+                  <div className="text-orange-500">Chat now</div>
+                </div>
+              </div>
+            </div>
             <ParentComponent>
-                <Details details={tour} />
-              </ParentComponent>
+              <Details details={tour} />
+            </ParentComponent>
           </div>
           <ParentComponent>
             <div
@@ -90,7 +110,7 @@ const TourDetails = () => {
             </div>
           </ParentComponent>
         </div>
-        )}
+      )}
     </div>
   );
 };
